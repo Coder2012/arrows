6e00a50013302fe4a2d41377fe3636629c4e3d60
Neil Brown <nbrown@amaze.com>
2016-01-30 20:42:25 +0000

    added promise based loading via jquery deferred, added textures to the floor

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var scene, camera, renderer;
 var plane, cylinder, box, arrow;
 
+var textures = [], models = [], deps = [];
+
 var velocity = new THREE.Vector3(0, 35, -70);
-var gravity = new THREE.Vector3(0, -10, 0);
+var gravity = new THREE.Vector3(0, -10, 10);
 var position = new THREE.Vector3(0, 0, 10000);
 var centre = new THREE.Vector3(0, 0, 0);
 var up = new THREE.Vector3(0, 1, 0);
@@ -15,13 +17,52 @@ var pt, radians, axis, tangent;
 
 var clock = new THREE.Clock();
 
+
+
 init();
 
 function init(){
 	setupScene();
-	createObjects();
 
 	loadModel();
+	loadItems();
+
+	$.when.apply(this, deps).done(function(){
+		console.log("loaded: ", textures);
+
+		createObjects();
+		render();
+	});
+}
+
+function loadItems(){
+	deps.push(loadTexture('metal_diffuse', 'metal_diffuse.jpg'));
+	deps.push(loadTexture('metal_specular', 'metal_specular.jpg'));
+	deps.push(loadTexture('metal_normal', 'metal_normal.jpg'));
+}
+
+function loadTexture(name, path){
+	var deferred = $.Deferred();
+
+	var loader = new THREE.TextureLoader();
+	loader.load('../img/' + path, function(texture){
+		textures[name] = texture;
+		deferred.resolve();
+	});
+
+	return deferred.promise();
+}
+
+function loadModel(name, path){
+	var deferred = $.Deferred();
+
+	var loader = new THREE.JSONLoader();
+	loader.load('../model/' + path, function(geometry){
+		models[name] = geometry;
+		deferred.resolve();
+	});
+
+	return deferred.promise();
 }
 
 function loadModel(){
@@ -33,9 +74,7 @@ function loadModel(){
 		geometry.rotateZ(THREE.Math.degToRad(90));
 		arrow.scale.set(scale, scale, scale);
 		scene.add(arrow);
-		
-		render();
-	})
+	});
 }
 
 var update = function(y, z){
@@ -59,15 +98,42 @@ function setupScene(){
 	camera.position.z = 12000;
 	camera.lookAt(scene.position);
 
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+	directionalLight.position.set( 10, 10, 0 );
+	scene.add( directionalLight );
+
 	renderer = new THREE.WebGLRenderer({antialias: true});
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 }
 
 function createObjects(){
-	var geometry = new THREE.PlaneGeometry(2000, 20000);
-	var material = new THREE.MeshBasicMaterial({color: 0x884422});
-	plane = new THREE.Mesh(geometry, material);
+	var geometry = new THREE.PlaneGeometry(20000, 20000, 1, 1);
+
+	var repeat = 5;
+	floorMaterial = new THREE.MeshPhongMaterial( {
+		color: 0xdddddd,
+		specular: 0x222222,
+		shininess: 55,
+		map: textures['metal_diffuse'],
+		specularMap: textures['metal_specular'],
+		normalMap: textures['metal_normal'],
+		normalScale: new THREE.Vector2( 1, 1 )
+	} );
+
+	floorMaterial.map.wrapS = THREE.RepeatWrapping;
+	floorMaterial.map.wrapT = THREE.RepeatWrapping;
+	floorMaterial.map.repeat.set(repeat, repeat);
+
+	floorMaterial.specularMap.wrapS = THREE.RepeatWrapping;
+	floorMaterial.specularMap.wrapT = THREE.RepeatWrapping;
+	floorMaterial.specularMap.repeat.set(repeat, repeat);
+
+	floorMaterial.normalMap.wrapS = THREE.RepeatWrapping;
+	floorMaterial.normalMap.wrapT = THREE.RepeatWrapping;
+	floorMaterial.normalMap.repeat.set(repeat, repeat);
+
+	plane = new THREE.Mesh(geometry, floorMaterial);
 	plane.rotateOnAxis(new THREE.Vector3(1, 0, 0), THREE.Math.degToRad(-90));
 	plane.name = 'plane';
 	scene.add(plane);
